b897fbd8681b0cdb5eb0396c2131671069523b91
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2022-08-13 19:45:10 -0230

    game words will not repeat

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Hangman {
-    public void outputWord(String word, UpdatedString updates, ArrayList<String> guesses, ArrayList<String> word_guesses){
+    private void outputWord(String word, UpdatedString updates, ArrayList<String> guesses, ArrayList<String> word_guesses){
 
         for(char x : word.toCharArray()) {
             Boolean present = false;
@@ -46,79 +43,93 @@ public class Hangman {
 
     public void playHangman(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Begin Game? Y/N");
         try {
-            String input = scanner.next();
-            if (input.equalsIgnoreCase("y")) {
-                System.out.println("-----------------------------------------Instructions-----------------------------------------\n----------------------Guess using a letter or word, or guess the answer!----------------------\n-----If you guess a word, you can still guess the letters within that word on other turns-----");
-                StringBuilder sb = new StringBuilder();
-                BufferedReader br = new BufferedReader(new FileReader("visuals.txt"));
-                String line = br.readLine();
-                while (line != null) {
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
-                    line = br.readLine();
-                }
-                LinkedList<String> visualsList = new LinkedList<>(Arrays.asList(sb.toString().split(",")));
-                System.out.println(visualsList.get(0));
-//               Get word
-                String word = Words.getWord().toUpperCase();
-                for (char x : word.toCharArray()) {
-                    if (String.valueOf(x).equals(" ") || String.valueOf(x).equals(".") || String.valueOf(x).equals(":") || String.valueOf(x).equals("-") || String.valueOf(x).equals("'") ||
-                            String.valueOf(x).matches("\\d")) {
-                        System.out.print(x);
-                    } else {
-                        System.out.print("_");
+            boolean good = false;
+            do{
+                System.out.println("Begin Game? Y/N");
+                String input = scanner.next();
+
+                if (input.equalsIgnoreCase("y")) {
+                    good = true;
+                    System.out.println("-----------------------------------------Instructions-----------------------------------------\n----------------------Guess using a letter or word, or guess the answer!----------------------\n-----If you guess a word, you can still guess the letters within that word on other turns-----");
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new FileReader("visuals.txt"));
+                    String line = br.readLine();
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
                     }
-                }
-                System.out.println("\n"+word);
+                    LinkedList<String> visualsList = new LinkedList<>(Arrays.asList(sb.toString().split(",")));
+                    System.out.println(visualsList.get(0));
+//               Get word
+                    FileWriter used_words = new FileWriter("used_words.txt");
+                    String word = Words.getWord().toUpperCase();
+                    used_words.write(word+"\n");
+                    used_words.close();
+                    for (char x : word.toCharArray()) {
+                        if (String.valueOf(x).equals(" ") || String.valueOf(x).equals(".") || String.valueOf(x).equals(":") || String.valueOf(x).equals("-") || String.valueOf(x).equals("'") ||
+                                String.valueOf(x).matches("\\d")) {
+                            System.out.print(x);
+                        } else {
+                            System.out.print("_");
+                        }
+                    }
+
+//                    System.out.println("\n"+word);
 
 //               Start guessing
-                System.out.print("\nStart Guessing!\n");
-                Boolean game = true;
+                    System.out.print("\nStart Guessing!\n");
+                    Boolean game = true;
 
-                ArrayList<String> guesses = new ArrayList<>();
-                ArrayList<String> word_guesses = new ArrayList<>();
-                do {
-                    UpdatedString updates = new UpdatedString();
-                    String guess = scanner.nextLine().toUpperCase();
+                    ArrayList<String> guesses = new ArrayList<>();
+                    ArrayList<String> word_guesses = new ArrayList<>();
+                    do {
+                        UpdatedString updates = new UpdatedString();
+                        String guess = scanner.nextLine().toUpperCase();
 
 //                    check if previously guessed
-                    Boolean present = false;
-                    for(String g : guesses){
-                        if(g.contains(guess)){
-                            present = true;
+                        Boolean present = false;
+                        for(String g : guesses){
+                            if(g.contains(guess)){
+                                present = true;
+                            }
                         }
-                    }
 
-                    if (present==true) {
-                        System.out.println("Already used. Try again!\n");
+                        if (present==true) {
+                            System.out.println("Already used. Try again!\n");
 
-                    } else if (present==false && word.contains(guess)) {
+                        } else if (present==false && word.contains(guess)) {
 //                       System.out.println("present!");
-                        if(guess.length()>1){
-                            System.out.println(visualsList.get(0));
-                            word_guesses.add(guess);
-                            outputWord(word, updates, guesses, word_guesses);
-                        }
-                        else{
-                            System.out.println(visualsList.get(0));
-                            guesses.add(guess);
-                            outputWord(word, updates, guesses, word_guesses);
-                        }
+                            if(guess.length()>1){
+                                System.out.println(visualsList.get(0));
+                                word_guesses.add(guess);
+                                outputWord(word, updates, guesses, word_guesses);
+                            }
+                            else{
+                                System.out.println(visualsList.get(0));
+                                guesses.add(guess);
+                                outputWord(word, updates, guesses, word_guesses);
+                            }
 
-                    } else {
-                        System.out.println("Not Present!");
-                        visualsList.pop();
-                        System.out.println(visualsList.get(0));
-                        outputWord(word, updates, guesses, word_guesses);
-                        if (visualsList.size() == 2) {
-                            game = false;
-                            System.out.println("Answer: " + word);
+                        } else {
+                            System.out.println("Not Present!");
+                            visualsList.pop();
+                            System.out.println(visualsList.get(0));
+                            outputWord(word, updates, guesses, word_guesses);
+                            if (visualsList.size() == 2) {
+                                game = false;
+                                System.out.println("Answer: " + word);
+                            }
                         }
-                    }
-                } while (game == true);
-            }
+                    } while (game == true);
+                }else if(input.equalsIgnoreCase("n")){
+                    System.exit(1);
+                }else{
+                    System.out.println("Incorrect input");
+                }
+            } while(good==false);
+
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/Words.java
+++ src/Words.java
@@ -10,6 +10,14 @@ public class Words {
 
     public static String getWord() throws IOException {
         LinkedList<String> words= new LinkedList<String>();
+        LinkedList<String> used_words = new LinkedList<>();
+        try(BufferedReader word_check = new BufferedReader(new FileReader("used_words.txt"))){
+            String word = word_check.readLine();
+            while(word!=null){
+                used_words.add(word);
+                word = word_check.readLine();
+            }
+        }
         try(BufferedReader br = new BufferedReader(new FileReader("words.txt"))){
             String word = br.readLine();
             while(word!=null){
@@ -21,7 +29,17 @@ public class Words {
             fe.printStackTrace();
         }
         Random index = new Random();
-        return words.get(index.nextInt(words.size()));
+        String game_word;
+        boolean no_repeat = false;
+        do{
+            game_word = words.get(index.nextInt(words.size()));
+
+            if (!used_words.contains(game_word)){
+                no_repeat=true;
+            }
+        }while(no_repeat = false);
+
+        return game_word;
     }
 
 }
